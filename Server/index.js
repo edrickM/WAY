bfa4e68f675f530f8a48305f66d0c70cf4fafc73
edrickM <Edrickmiranda@gmail.com>
2022-10-17 15:33:17 -0500

    random markers added for testing

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
 const colors = require('./colors')
 const cors = require('cors');
 const SCALEDRONE_CHANNEL_ID = ('PMPs48ZjR8VGrTSE');
@@ -42,45 +41,4 @@ app.post('/auth', (req, res) => {
         res.send(token);
     } 
 );
-
-//If clientId and name are valid, we generate a token with clients data
-// app.post(
-//   'https://2274-2605-ad80-30-841a-5003-bd0a-46cc-8ac.ngrok.io',
-//   (req, res) => {
-//     let {clientId, name} = req.body;
-//     if (!clientId || clientId.lenght < 1) {
-//       res.status(400).send('Invalid Id');
-//     }
-//     if (!name || name.lenght < 1) {
-//       res.status(400).send('Invalid Name');
-//     }
-//     var token = jwt.sign(
-//       {
-//         client: clientId,
-//         channel: SCALEDRONE_CHANNEL_ID,
-//         data: {
-//           name,
-//         },
-//         Permissions: {
-//           '^obserbable-locations$': {
-//             publish: true,
-//             subscribe: true,
-//             history: 50,
-//           },
-//         },
-//         exp: Math.floor(Date.now() / 1000) + 60 * 5,
-//       },
-//       SCALEDRONE_CHANNEL_SECRET,
-//     );
-
-//     if (err) {
-//       const error = new Error("Error! Something went wrong.");
-//       return next(error);
-//     }
-
-//     res.send(token);
-
-//   },
-// );
-
 app.listen(19002, () => console.log('Server listening on port 19002'));
---
+++ Server/random_markers.js
@@ -0,0 +1,67 @@
+const Scaledrone = require('scaledrone-node');
+const fetch = require('node-fetch');
+const SCALEDRONE_CHANNEL_ID = ('PMPs48ZjR8VGrTSE');
+
+const locations = [
+  {name: 'Bob', longitude: -97.319601, latitude: 32.7399279},
+  {name: 'Alice', longitude: -97.055798, latitude: 32.912450},
+  {name: 'John', longitude: -96.788884, latitude: 32.756097},
+];
+
+function doAuthRequest(clientId, name) {
+  let status;
+  return fetch('https://69da-2605-ad80-30-841a-e4ab-2e1e-f3a5-a8c.ngrok.io/auth', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({clientId, name})
+  }).then(res => {
+    status = res.status;
+    return res.text();
+  }).then(text => {
+    if (status === 200) {
+      return text;
+    } else {
+      console.error(text);
+    }
+  }).catch(error => console.error(error));
+}
+
+locations.forEach(location => {
+  const drone = new Scaledrone(SCALEDRONE_CHANNEL_ID);
+  drone.on('error', error => console.error(error));
+  drone.on('open', error => {
+    if (error) {
+      return console.error(error);
+    }
+    doAuthRequest(drone.clientId, location.name)
+      .then(jwt => drone.authenticate(jwt));
+  });
+  drone.on('authenticate', error => {
+    if (error) {
+      return console.error(error);
+    }
+    setInterval(() => {
+      const delta = moveInRandomDirection();
+      location.latitude += delta.dlat;
+      location.longitude += delta.dlon;
+      drone.publish({
+        room: 'observable-locations',
+        message: location
+      });
+    }, 1000);
+  });
+  // subscribe so our data is avaiable to the observable room
+  drone.subscribe('observable-locations');
+});
+
+function moveInRandomDirection(maxDistance = 0.005) {
+  const angle = Math.random() * Math.PI * 2;
+  const distance = maxDistance * Math.random();
+  return {
+    dlat: Math.cos(angle) * distance,
+    dlon: Math.sin(angle) * distance,
+  };
+}
